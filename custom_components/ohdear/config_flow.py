4db224852fcf327a45fc533bb55d828f6faa0e6c
Owen Voke <development@voke.dev>
2023-07-05 17:22:35 +0100

    feat: add support for updating interval

--- custom_components/ohdear/config_flow.py
+++ custom_components/ohdear/config_flow.py
@@ -1,15 +1,20 @@
 from typing import Any
 
+from datetime import timedelta
 import voluptuous as vol
+import logging
 
-from homeassistant.config_entries import ConfigFlow
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
 from homeassistant.const import CONF_API_TOKEN, CONF_SCAN_INTERVAL
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from ohdear import OhDear, NotFoundException, UnauthorizedException
 
 import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, CONF_SITE_ID, DEFAULT_SCAN_INTERVAL
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -25,9 +30,7 @@ CONFIG_SCHEMA = vol.Schema(
 class OhDearConfigFlow(ConfigFlow, domain=DOMAIN):
     """The configuration flow for an Oh Dear system."""
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input=None) -> FlowResult:
         """Ask the user for an API token, site id, and a name for the system."""
         errors = {}
         if user_input:
@@ -56,6 +59,51 @@ class OhDearConfigFlow(ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: ConfigEntry) -> OhDearOptionsFlowHandler:
+        return OhDearOptionsFlowHandler(config_entry)
+
+
+class OhDearOptionsFlowHandler(OptionsFlow):
+    """Config flow options handler for Oh Dear."""
+
+    def __init__(self, config_entry: ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+        # Cast from MappingProxy to dict to allow update.
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            self.options.update(user_input)
+            coordinator = self.hass.data[DOMAIN][self.config_entry.entry_id]
+
+            update_interval = timedelta(
+                seconds=self.options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
+            )
+
+            _LOGGER.debug("Updating coordinator, update_interval: %s", update_interval)
+
+            coordinator.update_interval = update_interval
+
+            return self.async_create_entry(title="", data=self.options)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL,
+                        default=self.config_entry.options.get(
+                            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+                        ),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=1)),
+                }
+            ),
         )
 
 
